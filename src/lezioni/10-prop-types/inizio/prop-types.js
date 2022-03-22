2804a224c5eadaf33c58323640ef34d4c944a22c
Simone_Berti <foto.berti55@gmail.com>
2022-03-22 19:14:38 +0100

    Props Types lezione 10

--- src/lezioni/10-prop-types/inizio/prop-types.js
+++ src/lezioni/10-prop-types/inizio/prop-types.js
@@ -1,12 +1,51 @@
 import React from "react";
 import { datiIncompleti } from "../../../data";
+import PropTypes from "prop-types";
+
+const imageDefoult =
+  "https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png";
 
 const PropComponent = () => {
   return (
     <div>
-      <h3>Prop Types Component</h3>
+      {datiIncompleti.map((el) => {
+        // console.log(el);
+        return <Prodotto key={el.id} {...el} />;
+      })}
     </div>
   );
+};
+
+const Prodotto = ({ nome, prezzo, image }) => {
+  // console.log(nome, prezzo, image);
+  const imgi = image && image.url;
+  return (
+    <article className="shadow card my-3 p-2">
+      Singolo componente
+      <div className="card-body">
+        <img
+          src={imgi || imageDefoult}
+          alt={nome || "divano default"}
+          className="card-img-top"
+        />
+        <div className="card-text">
+          <h4 className="card-title">{nome || "divano default"}</h4>
+          <p className="card-subtitle">€{prezzo || 19.99}</p>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+Prodotto.prototype = {
+  nome: PropTypes.string.isRequired,
+  image: PropTypes.object.isRequired,
+  prezzo: PropTypes.number.isRequired,
+};
+
+Prodotto.defaultProps = {
+  nome: "divano",
+  prezzo: 17.99,
 };
 
 export default PropComponent;
---
+++ src/lezioni/10-prop-types/inizio/test.testing.js
@@ -0,0 +1,15 @@
+// import React from 'react'
+// import PropTypes from 'prop-types'
+
+// const test.testing = props => {
+//   return (
+//     <div>test.testing</div>
+//   )
+// }
+
+// test.testing.propTypes = {}
+
+// export default test.testing
+
+//comando per creare un compoennte con gia le props 
+// rafcp 
